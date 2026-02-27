c6e5a9160f6f7a905601a445f07b02a738c8d693
Scott Nicholes <scott93gps@gmail.com>
2026-02-26 16:18:26 -0800

    Initial Commit

---
+++ hello_rust/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hola!");
+    println!("Buongiorno!");
+    println!("Hello, world!");
+}
---
+++ treehouse/src/main.rs
@@ -0,0 +1,52 @@
+use std::io::stdin;
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_lowercase(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
+fn prompt_name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read name");
+    your_name.trim().to_lowercase()
+}
+
+fn main() {
+    let visitor_list = [
+        Visitor::new("echo", "Hello, Echo!"),
+        Visitor::new("scott", "Hola, Scott!"),
+        Visitor::new("snoopy", "What's up snoop dog?"),
+    ];
+
+    println!("Hello, what is your name?");
+    let name = prompt_name();
+
+    let mut come_in = false;
+
+    for visitor in &visitor_list {
+        if visitor == &name {
+            come_in = true;
+        }
+    }
+
+    if come_in {
+        println!("Good Job!{}", name);
+    } else {
+        println!("Go away!!{}", name);
+    }
+}
